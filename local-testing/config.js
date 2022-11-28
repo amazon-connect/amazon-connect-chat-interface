2cc5e4dd530e0fe7f2d2b91eb0e4698e92fd8ade
Spencer Lepine <spenlep@amazon.com>
2022-11-27 19:22:24 -0800

    revert example config to have placeholder value

--- local-testing/config.js
+++ local-testing/config.js
@@ -1,7 +1,7 @@
 var contactFlowId = "12345678-1234-1234-1234-123456789012"; // TODO: Fill in
 var instanceId = "12345678-1234-1234-1234-123456789012"; // TODO: Fill in
 var apiGatewayEndpoint = "https://<api-id>.execute-api.<region>.amazonaws.com/<stage>/"; // TODO: Fill in with the API Gateway endpoint created by your CloudFormation template
-var region = "us-west-2"; // TODO: Fill in
+var region = "<regoin>"; // TODO: Fill in
 
 var initConfig = {
     name: "John",
