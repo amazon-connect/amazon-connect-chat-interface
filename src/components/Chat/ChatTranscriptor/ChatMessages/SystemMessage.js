d57dcacfd1a31b548ef65ef0e8a7d42de8592657
John Xia <xiajon@amazon.com>
2023-12-06 19:43:23 +0000

    add idle events

--- src/components/Chat/ChatTranscriptor/ChatMessages/SystemMessage.js
+++ src/components/Chat/ChatTranscriptor/ChatMessages/SystemMessage.js
@@ -13,19 +13,20 @@ export class SystemMessage extends React.PureComponent {
   static defaultProps = {};
 
   getMessageText = () => {
-    console.log("SystemMessage getMessageText");
-    console.log(this.props);
-    let name  = "";
+    let name = this.props.messageDetails.displayName;
     switch (this.props.messageDetails.content.type) {
       case ContentType.EVENT_CONTENT_TYPE.PARTICIPANT_JOINED:
-        name = this.props.messageDetails.displayName;
-        return name + " has joined the chat";
+        return `${name} has joined the chat`;
+      case ContentType.EVENT_CONTENT_TYPE.PARTICIPANT_IDLE:
+        return `${name} has become idle`;
+      case ContentType.EVENT_CONTENT_TYPE.PARTICIPANT_DISCONNECT:
+        return `${name} has been idle too long, disconnecting`;
+      case ContentType.EVENT_CONTENT_TYPE.PARTICIPANT_RETURNED:
+        return `${name} has returned`;
       case ContentType.EVENT_CONTENT_TYPE.PARTICIPANT_LEFT:
-        name = this.props.messageDetails.displayName;
-        return name + " has left the chat";
+        return `${name} has left the chat`;
       case ContentType.EVENT_CONTENT_TYPE.CHAT_ENDED:
-        return "Chat has ended!";
-
+        return `Chat has ended!`;
       default:
         return "";
     }
--- src/components/Chat/datamodel/Model.js
+++ src/components/Chat/datamodel/Model.js
@@ -11,36 +11,45 @@ export const PARTICIPANT_TYPES = {
 };
 
 export const ContentType = {
-  EVENT_CONTENT_TYPE: {
-    TYPING: "application/vnd.amazonaws.connect.event.typing",
-    READ_RECEIPT: "application/vnd.amazonaws.connect.event.message.read",
-    DELIVERED_RECEIPT: "application/vnd.amazonaws.connect.event.message.delivered",
-    PARTICIPANT_JOINED: "application/vnd.amazonaws.connect.event.participant.joined",
-    PARTICIPANT_LEFT: "application/vnd.amazonaws.connect.event.participant.left",
-    TRANSFER_SUCCEEDED: "application/vnd.amazonaws.connect.event.transfer.succeeded",
-    TRANSFER_FAILED: "application/vnd.amazonaws.connect.event.transfer.failed",
-    CONNECTION_ACKNOWLEDGED: "application/vnd.amazonaws.connect.event.connection.acknowledged",
-    CHAT_ENDED: "application/vnd.amazonaws.connect.event.chat.ended",
-  },
-  MESSAGE_CONTENT_TYPE: {
-    TEXT_PLAIN: "text/plain",
-    TEXT_MARKDOWN: "text/markdown",
-    APPLICATION_PDF: "application/pdf",
-    IMAGE_JPG: "image/jpeg",
-    IMAGE_PNG: "image/png",
-    APPLICATION_DOC: "application/msword",
-    APPLICATION_DOCX: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-    APPLICATION_XLS: "application/vnd.ms-excel",
-    APPLICATION_XLSX: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-    TEXT_CSV: "text/csv",
-    APPLICATION_PPT: "application/vnd.ms-powerpoint",
-    APPLICATION_PPTX: "application/vnd.openxmlformats-officedocument.presentationml.presentation",
-    AUDIO_WAV: "audio/wav",
-    AUDIO_X_WAV: "audio/x-wav", //Firefox
-    AUDIO_VND_WAVE: "audio/vnd.wave", //IE
-    INTERACTIVE_MESSAGE: "application/vnd.amazonaws.connect.message.interactive",
-    INTERACTIVE_RESPONSE: "application/vnd.amazonaws.connect.message.interactive.response",
-  },
+    EVENT_CONTENT_TYPE: {
+        TYPING: "application/vnd.amazonaws.connect.event.typing",
+        READ_RECEIPT: "application/vnd.amazonaws.connect.event.message.read",
+        DELIVERED_RECEIPT: "application/vnd.amazonaws.connect.event.message.delivered",
+        PARTICIPANT_JOINED: "application/vnd.amazonaws.connect.event.participant.joined",
+        PARTICIPANT_LEFT: "application/vnd.amazonaws.connect.event.participant.left",
+        PARTICIPANT_IDLE: "application/vnd.amazonaws.connect.event.participant.idle",
+        PARTICIPANT_DISCONNECT: "application/vnd.amazonaws.connect.event.participant.autodisconnection",
+        PARTICIPANT_RETURNED: "application/vnd.amazonaws.connect.event.participant.returned",
+        TRANSFER_SUCCEEDED: "application/vnd.amazonaws.connect.event.transfer.succeed",
+        TRANSFER_FAILED: "application/vnd.amazonaws.connect.event.transfer.failed",
+        CONNECTION_ACKNOWLEDGED: "application/vnd.amazonaws.connect.event.connection.acknowledged",
+        CHAT_ENDED: "application/vnd.amazonaws.connect.event.chat.ended"
+    },
+    MESSAGE_CONTENT_TYPE: {
+        TEXT_PLAIN: "text/plain",
+        TEXT_MARKDOWN: "text/markdown",
+        APPLICATION_PDF: "application/pdf",
+        IMAGE_JPG: "image/jpeg", // JPEG image (.jpg .jpeg .jfif)
+        IMAGE_PNG: "image/png",
+        APPLICATION_DOC: "application/msword",
+        APPLICATION_DOCX: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        APPLICATION_XLS: "application/vnd.ms-excel",
+        APPLICATION_XLSX: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        TEXT_CSV: "text/csv",
+        APPLICATION_PPT: "application/vnd.ms-powerpoint",
+        APPLICATION_PPTX: "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+        AUDIO_WAV: "audio/wav",
+        AUDIO_X_WAV: "audio/x-wav", //Firefox
+        AUDIO_VND_WAVE: "audio/vnd.wave", //IE
+        INTERACTIVE_MESSAGE: "application/vnd.amazonaws.connect.message.interactive",
+        INTERACTIVE_RESPONSE: "application/vnd.amazonaws.connect.message.interactive.response",
+        VIDEO_QUICKTIME: "video/quicktime", // QuickTime file format (.mov)
+        RICH_TEXT: "text/richtext", // Rich text (.rtf)
+        RICH_TEST_FILE_RTF: "application/rtf", // Rich text (.rtf)
+        RICH_TEST_FILE_X_RTF: "application/x-rtf", // Rich text (.rtf)
+        VIDEO_MP4: "video/mp4", // MP4 Video (.mp4)
+        IMAGE_HEIC: "image/heic", // High Efficiency Image File (.heic)
+    }
 };
 
 ContentType.ATTACHMENT_CONTENT_TYPE = {
